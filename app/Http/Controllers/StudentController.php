c7b3ac4a8c31093ff7f1dae239655192596f08d3
Pritam Kumar Kundu <pritam.cse2014@gmail.com>
2024-12-21 18:09:41 +0600

    POST-API-With-Database-Insert-Data-In-DB-With-POST-API

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -13,6 +13,15 @@ class StudentController extends Controller
     }
 
     public function studentAdd(Request $request) {
-        return $request->input();
+        // return $request->input();
+        $student = new Student();
+        $student->name = trim($request->name);
+        $student->email = trim($request->email);
+        $student->phone = trim($request->phone);
+        if ($student->save()) {
+            return ["result" => "Student Added Successfully."];
+        } else {
+            return ["result" => "Student Added Failed."];
+        }
     }
 }
